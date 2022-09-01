4505d1377e57d810ec30e774ab333939988cfb3a
grakzanchetta <grakzanchetta@gmail.com>
2022-09-01 17:19:03 -0300

    template basico

--- src/repositories/businessRepository.ts
+++ src/repositories/businessRepository.ts
@@ -1,4 +1,4 @@
-import { connection } from "../database";
+import { connection } from "../database/database";
 import { TransactionTypes } from "./cardRepository";
 
 export interface Business {
--- src/repositories/cardRepository.ts
+++ src/repositories/cardRepository.ts
@@ -1,4 +1,4 @@
-import { connection } from "../database";
+import { connection } from "../database/database";
 import { mapObjectToUpdateQuery } from "../../utils/sqlUtils";
 
 export type TransactionTypes =

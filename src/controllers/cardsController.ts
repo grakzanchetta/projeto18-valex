e5710a4cb537dff0f708f41e10bd871d09c66656
grakzanchetta <grakzanchetta@gmail.com>
2022-09-03 01:48:44 -0300

    fix

--- src/controllers/cardsController.ts
+++ src/controllers/cardsController.ts
@@ -6,10 +6,9 @@ async function createCard(req: Request, res: Response) {
   if (!req.headers["x-api-key"]) return res.sendStatus(418);
   const apiKey = req.headers["x-api-key"].toString();
   const { employeeId, type } = req.body;
-  const id = Number(employeeId);
 
   companiesServices.validateApiKey(apiKey);
-  await cardsServices.createCard(id, type);
+  await cardsServices.createCard(Number(employeeId), type);
 
   res.sendStatus(201);
 }
